27691a6281e7fd5d4ac4b43a6953ec340769b62e
Massimiliano Cannarozzo <maxcanna@gmail.com>
2014-08-31 14:09:00 +0200

    Added cache control headers generation

--- src/BadgeController.php
+++ src/BadgeController.php
@@ -13,6 +13,7 @@ use Symfony\Component\HttpFoundation\Response;
 
 class BadgeController
 {
+    protected $app;
 
     public function __construct(Application $app)
     {
@@ -25,26 +26,31 @@ class BadgeController
         $lang = $this->app['request']->get('lang');
 
         if (!isset($packageid)) {
+            //TODO Return an image with message
             $this->app->abort(500, 'Invalid package id');
             return null;
         }
 
-        if (!$this->app['debug']) {
-            $res = null;
-            try {
-                $res = $this->app['guzzle']->get($this->app['ws.url'] . "?id={$packageid}&lang={$lang}", [
-                    $this->app['ws.auth.header.name'] => $this->app['ws.auth.header.value']
-                ])->send()->json();
-            } catch (ClientErrorResponseException $e) {
-                $this->app->abort($e->getResponse()->getStatusCode());
-                return null;
-            }
+        //TODO Generate 304 response if image is still valid!
 
-            $res = $this->app['guzzle']->get($res['icon'])->send();
-//TODO Add Cache-Control header se !debug
-            return new Response($res->getBody(), $res->getStatusCode(), ['content-type' => $res->getContentType()]);
+        $wsResponse = null;
+        try {
+            $wsResponse = $this->app['guzzle']->get($this->app['ws.url'] . "?id={$packageid}&lang={$lang}", [
+                $this->app['ws.auth.header.name'] => $this->app['ws.auth.header.value']
+            ])->send();
+        } catch (ClientErrorResponseException $e) {
+            //TODO Return an image with message
+            $this->app->abort($e->getResponse()->getStatusCode());
+            return null;
         }
 
-        return $this->app->sendFile(__DIR__ . '/../web/badge.png');
+        $imgResponse = $this->app['guzzle']->get($wsResponse->json()['icon'])->send();
+
+        return new Response($imgResponse->getBody(), $imgResponse->getStatusCode(), [
+            'Cache-Control' => $this->app['debug'] ? 'no-cache' : $wsResponse->getCacheControl(),
+            'Expires' => $this->app['debug'] ? '0' : $wsResponse->getExpires(),
+            'Last-Modified' => $wsResponse->getLastModified(),
+            'Content-Type' => $imgResponse->getContentType()
+        ]);
     }
 }
