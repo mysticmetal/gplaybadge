9188c667b21dd2e200a1a368f5d28d393f963810
Massimiliano Cannarozzo <maxcanna@gmail.com>
2014-10-29 00:34:30 +0100

    Using abs path

--- minify.sh
+++ minify.sh
@@ -1,10 +1,10 @@
-#js
-/usr/local/lib/node_modules/uglify-js/bin/uglifyjs web/static/life.js -o web/static/life.min.js --stats -v -c
+echo js
+uglifyjs web/static/life.js -o web/static/life.min.js --stats -v -c
 
-#html
+echo html
 htmlmin -o res/views/error.min.twig res/views/error.twig    
 #htmlmin -o res/views/analytics.min.twig res/views/analytics.twig 
 htmlmin -o res/views/home.min.twig res/views/home.twig
 
-#css
+echo css
 cleancss -d --s0 -o web/static/style.min.css web/static/style.css
